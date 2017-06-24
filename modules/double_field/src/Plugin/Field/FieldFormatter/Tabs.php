a655c96622fac12c51a0c04ec5352f476122f069
Dong Tian <dongtianzen@gmail.com>
2017-06-23 22:39:41 -0400

    install devel module

---
+++ modules/double_field/src/Plugin/Field/FieldFormatter/Tabs.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\double_field\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FieldItemListInterface;
+
+/**
+ * Plugin implementations for 'tabs' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "double_field_tabs",
+ *   label = @Translation("Tabs"),
+ *   field_types = {"double_field"}
+ * )
+ */
+class Tabs extends Base {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $settings = $this->getSettings();
+    $this->prepareItems($items);
+
+    $element[0] = [
+      '#theme' => 'double_field_tabs',
+      '#items' => $items,
+      '#settings' => $settings,
+      '#attached' => ['library' => ['double_field/tabs']],
+    ];
+
+    return $element;
+  }
+
+}
